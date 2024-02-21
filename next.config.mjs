8a04e8aeebe3cff99584bce0585450a8f745d083
jhonangelo <jhon@vananz.com>
2024-02-21 22:03:24 +0800

    add config env

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DIRECTUS_ENDPOINT: '',
-    DIRECTUS_USER_EMAIL: '',
-    DIRECTUS_USER_PASSWORD: '',
+    DIRECTUS_ENDPOINT: process.env.DIRECTUS_ENDPOINT,
+    DIRECTUS_USER_EMAIL: process.env.DIRECTUS_USER_EMAIL,
+    DIRECTUS_USER_PASSWORD: process.env.DIRECTUS_USER_PASSWORD,
+  },
+  images: {
+    domains: ['burst.shopifycdn.com'],
   },
 }
 
