24721a2446619f6bcee02e964b848f7c75a83e04
jhonangelo <jhon@vananz.com>
2024-02-21 22:03:39 +0800

    make loader not scrollable

--- src/app/Components/Loader.tsx
+++ src/app/Components/Loader.tsx
@@ -2,7 +2,7 @@ import { SyncLoader } from 'react-spinners'
 
 export default function Loader() {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className='flex justify-center items-center h-full min-h-[calc(100vh-64px)]'>
       <SyncLoader color='#36d7b7' />
     </div>
   )
